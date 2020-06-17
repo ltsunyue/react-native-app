4eadde6a95a5180a5ef77b7a9c06769bf28dd981
You <1135907864@qq.com>
2020-06-17 23:17:22 +0800

    增加首页状态栏透明

--- js/page/HomePage.js
+++ js/page/HomePage.js
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StatusBar, StyleSheet} from 'react-native';
 
 
 class HomePage extends Component {
     render() {
         return (
             <View>
-                <Text>HomePage</Text>
+                <StatusBar
+                    translucent={true}
+                    backgroundColor="transparent"
+                    barStyle="light-content"
+                />
+                <View style={styles.homeText}></View>
             </View>
         );
     }
 }
-
+const styles = StyleSheet.create({
+   homeText: {
+       height: 50,
+       backgroundColor: 'red'
+   }
+});
 export default HomePage;
